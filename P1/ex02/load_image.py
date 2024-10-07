28f6e34b8b36074fdd7060b6809d75be8d8d7bff
Belen Arbizu jimenez <barbizu-@c3r4s1.42malaga.com>
2024-10-07 19:21:13 +0200

    P1 ex02 init

---
+++ P1/ex02/load_image.py
@@ -0,0 +1,22 @@
+from PIL import Image, UnidentifiedImageError
+import numpy as np
+
+def ft_load(path: str) -> str:
+    """
+    Convert the image into an 8-bit numpy array and print the shape
+    and its pixels content in RGB format
+    """
+    try:
+        im = Image.open(path)
+    except FileNotFoundError:
+        raise SystemExit("File not found")
+    except UnidentifiedImageError:
+        raise SystemExit("Image can't be open and identified")
+    except ValueError:
+        raise SystemExit("Error")
+    except TypeError:
+        raise SystemExit("Format image is not correct")
+
+    img = np.asarray(im)
+    print("The shape of image is:", img.shape)
+    return str(img)
