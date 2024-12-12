3c71695bd4b632eca2ce6191a310e6f1ea730c56
Belen Arbizu jimenez <barbizu-@c2r8s5.42malaga.com>
2024-12-12 19:58:13 +0100

    zoom changed

--- P1/ex03/zoom.py
+++ P1/ex03/zoom.py
@@ -1,11 +1,16 @@
 from load_image import ft_load
-from scipy import ndimage, misc
+import cv2
 
 
 def zoom(path):
     print(ft_load(path))
     try:
         img = cv2.imread(path)
+        image_cropped = img[100:500, 400:800]
+        print("New shape after slicing:", image_cropped.shape)
+        cv2.imshow('Imagen redimensionada', image_cropped)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
     except:
         print("Error")
 
