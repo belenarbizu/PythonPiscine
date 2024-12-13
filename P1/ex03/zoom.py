9f6a9bca74efd9e5fd79fc96006b3f87dc48f9ed
belen-applymed <158149022+belen-applymed@users.noreply.github.com>
2024-12-13 11:50:51 +0100

    ex03 done

--- P1/ex03/zoom.py
+++ P1/ex03/zoom.py
@@ -1,13 +1,20 @@
 from load_image import ft_load
-from scipy import ndimage, misc
-
+import cv2
+import numpy as np
 
 def zoom(path):
-    print(ft_load(path))
     try:
+    º   print(ft_load(path))
         img = cv2.imread(path)
-    except:
-        print("Error")
+        img_zoom = img[100:500, 150:550]
+        #Cambio la imagen RGB de 3 canales a un solo canal (400,400)
+        img_gray = cv2.cvtColor(img_zoom, cv2.COLOR_RGB2GRAY)
+        #Expandimos un canal adicional (400,400,1)
+        img_gray_expanded = np.expand_dims(img_gray, axis=-1) 
+        print("New shape after slicing:", img_gray_expanded.shape)
+        print(img_gray_expanded)
+    except Exception as e:
+        print("Error:", e)
 
 
 def main():
