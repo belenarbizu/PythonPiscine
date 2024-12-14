9c88eed7395e437d9ac66a018d90f596a5058ce0
belen-applymed <158149022+belen-applymed@users.noreply.github.com>
2024-12-14 17:19:05 +0100

    ex05 init

---
+++ P1/ex05/load_image.py
@@ -0,0 +1,25 @@
+from PIL import Image, UnidentifiedImageError
+import numpy as np
+import cv2
+
+
+def ft_load(path: str) -> str:
+    """
+    Convert the image into an 8-bit numpy array and print the shape
+    and its pixels content in RGB format
+    """
+    try:
+        im = Image.open(path)
+    except FileNotFoundError:
+        raise SystemExit("File not found")
+    except UnidentifiedImageError:
+        raise SystemExit("Image can't be open and identified")
+    except ValueError:
+        raise SystemExit("Can't read file")
+    except TypeError:
+        raise SystemExit("Format image is not correct")
+
+    img = np.asarray(im)
+    print("The shape of image is:", img.shape)
+    print(img)
+    return str(img)
---
+++ P1/ex05/pimp_image.py
@@ -0,0 +1,29 @@
+def ft_invert(array)-> array:
+    """
+    Inverts the color of the image received.
+    """
+    #your code here
+
+def ft_red(array)-> array:
+    """
+    Changes the image to red.
+    """
+    #your code here
+
+def ft_green(array)-> array:
+    """
+    Changes the image to green.
+    """
+    #your code here
+
+def ft_blue(array)-> array:
+    """
+    Changes the image to blue.
+    """
+    #your code here
+
+def ft_grey(array)-> array:
+    """
+    Changes the image to grey.
+    """
+    #your code here
