15b6db82db6964528fc48fb74f8c0341a4f6bae6
belen-applymed <158149022+belen-applymed@users.noreply.github.com>
2024-12-15 17:32:40 +0100

    p2 ex00 done

---
+++ P2/ex00/load_csv.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from pandas.errors import EmptyDataError, ParserError
+
+def load(path: str)-> pd.DataFrame:
+    try:
+        dataset = pd.read_csv(path)
+        print("Loading dataset of dimensions", dataset.shape)
+        return dataset
+    except FileNotFoundError:
+        print("Error: File not found")
+    except PermissionError:
+        print("Error: Don't have permission to open the file")
+    except EmptyDataError:
+        print("Error: The file is empty")
+    except ParserError:
+        print("Error: The file doesn't have a valid format")
+    return None
