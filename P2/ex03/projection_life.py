441948203d42f1709e8d3b775ac0fe43c734c202
belen-applymed <158149022+belen-applymed@users.noreply.github.com>
2024-12-16 11:12:17 +0100

    p2 ex03 done

--- P2/ex03/projection_life.py
+++ P2/ex03/projection_life.py
@@ -5,7 +5,13 @@ def population_life(life_path: str, income_path: str) -> None:
     life_df = load(life_path)
     income_df = load(income_path)
 
+    life_1900 = life_df["1900"]
+    print(life_1900)
+    income_1900 = income_df["1900"]
 
+    plt.plot(income_1900, life_1900, "ro", color="blue")
+    plt.xscale('log')
+    plt.xticks([300, 1000, 10000], ["300", "1k", "10k"])
     plt.xlabel("Gross domestic product")
     plt.ylabel("Life Expectancy")
     plt.title("1900")
