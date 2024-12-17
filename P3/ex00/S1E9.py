154c22b50a33688b9e747bbcf7d8c0967812ddf0
Belen Arbizu jimenez <barbizu-@c1r11s4.42malaga.com>
2024-12-17 19:40:24 +0100

    p3 ex00 abc init

--- P3/ex00/S1E9.py
+++ P3/ex00/S1E9.py
@@ -1,10 +1,27 @@
 from abc import ABC, abstractmethod
 
 class Character(ABC):
-    """Your docstring for Class"""
+    """
+    Abstract class
+    """
     @abstractmethod
-    #your code here
+    def die(self):
+        pass
 
 class Stark(Character):
-    """Your docstring for Class"""
-    #your code here
+    """
+    Inherits from Character
+    """
+    def __init__(self, name, is_alive=True):
+        """
+        name mandatory
+        is_alive default True, non mandatory
+        """
+        self.name = name
+        self.is_alive = is_alive if is_alive is True else False
+    
+    def die(self):
+        """
+        Changes is_alive value from True to False
+        """
+        self.is_alive = False
