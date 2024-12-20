9be2f2a7edbf9c87cc4887458dff3d7d446c2bc6
belen-applymed <158149022+belen-applymed@users.noreply.github.com>
2024-12-20 11:19:33 +0100

    p4 ex03 done

--- P4/ex03/new_student.py
+++ P4/ex03/new_student.py
@@ -3,8 +3,26 @@ import string
 from dataclasses import dataclass, field
 
 def generate_id() -> str:
+    """
+    Generate a random id
+    """
     return "".join(random.choices(string.ascii_lowercase, k = 15))
 
 @dataclass
 class Student:
-    #your code here
+    """
+    Dataclass with name, surname, active, login and id info.
+    Name and surname are mandatory in init.
+    Active: default True.
+    login is generate with first letter of name + surname.
+    id is created with generate_id() function.
+    """
+    name: str
+    surname: str
+    active: bool = field(default=True, init=False)
+    login: str = field(init=False)
+    id: str = field(init=False)
+
+    def __post_init__(self):
+        self.login = self.name[0] + self.surname
+        self.id = generate_id()
