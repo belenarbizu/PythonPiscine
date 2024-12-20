b0a061e6225d98932b1c8866b8f19eeb74720545
belen-applymed <158149022+belen-applymed@users.noreply.github.com>
2024-12-20 09:20:32 +0100

    ex00 p4 done

--- P4/ex00/statistics.py
+++ P4/ex00/statistics.py
@@ -1,9 +1,12 @@
 def mean(args):
-    sum_arg = 0
-    for arg in args:
-        sum_arg += arg
-    result = sum_arg / len(args)
-    print("mean :", result)
+    if len(args) == 0:
+        print("ERROR")
+    else:
+        sum_arg = 0
+        for arg in args:
+            sum_arg += arg
+        result = sum_arg / len(args)
+        print("mean :", result)
 
 def bubbleSort(args):
     sorted_num = list(args)
@@ -17,45 +20,57 @@ def bubbleSort(args):
     return tuple(sorted_num)
 
 def median(args):
-    sorted_num = bubbleSort(args)
-    if len(sorted_num) % 2 == 0:
-        num_1 = sorted_num[int(len(sorted_num) / 2) - 1]
-        num_2 = sorted_num[int(len(sorted_num) / 2)]
-        median = (num_1 + num_2) / 2
+    if len(args) == 0:
+        print("ERROR")
     else:
-        median = sorted_num[int(len(sorted_num) / 2)]
-    print("median :", median)
+        sorted_num = bubbleSort(args)
+        if len(sorted_num) % 2 == 0:
+            num_1 = sorted_num[int(len(sorted_num) / 2) - 1]
+            num_2 = sorted_num[int(len(sorted_num) / 2)]
+            median = (num_1 + num_2) / 2
+        else:
+            median = sorted_num[int(len(sorted_num) / 2)]
+        print("median :", median)
 
 def quartile(args):
-    sorted_num = bubbleSort(args)
-    quartile = [0.0, 0.0]
-    first = len(args) / 4
-    last = 3 * len(args) / 4
-    quartile[0] = float(sorted_num[int(first)])
-    quartile[1] = float(sorted_num[int(last)])
-    print("quartile :", quartile)
+    if len(args) == 0:
+        print("ERROR")
+    else:
+        sorted_num = bubbleSort(args)
+        quartile = [0.0, 0.0]
+        first = len(args) / 4
+        last = 3 * len(args) / 4
+        quartile[0] = float(sorted_num[int(first)])
+        quartile[1] = float(sorted_num[int(last)])
+        print("quartile :", quartile)
 
 def std(args):
-    sum_arg = 0.0
-    for arg in args:
-        sum_arg += arg
-    mean = sum_arg / len(args)
-    std = 0.0
-    for arg in args:
-        std += (arg - mean) ** 2
-    std = (std / len(args)) ** 0.5
-    print("std :", std)
+    if len(args) == 0:
+        print("ERROR")
+    else:
+        sum_arg = 0.0
+        for arg in args:
+            sum_arg += arg
+        mean = sum_arg / len(args)
+        std = 0.0
+        for arg in args:
+            std += (arg - mean) ** 2
+        std = (std / len(args)) ** 0.5
+        print("std :", std)
 
 def var(args):
-    sum_arg = 0.0
-    for arg in args:
-        sum_arg += arg
-    mean = sum_arg / len(args)
-    var = 0.0
-    for arg in args:
-        var += (arg - mean) ** 2
-    var = var / len(args)
-    print("var :", var)
+    if len(args) == 0:
+        print("ERROR")
+    else:
+        sum_arg = 0.0
+        for arg in args:
+            sum_arg += arg
+        mean = sum_arg / len(args)
+        var = 0.0
+        for arg in args:
+            var += (arg - mean) ** 2
+        var = var / len(args)
+        print("var :", var)
 
 def ft_statistics(*args: any, **kwargs: any)-> None:
     stats = ["mean", "median", "quartile", "std", "var"]
@@ -66,12 +81,6 @@ def ft_statistics(*args: any, **kwargs: any)-> None:
         "std": std,
         "var": var
     }
-    if len(args) == 0 or len(kwargs) == 0:
-        print("ERROR")
-    else:
-        for key in kwargs.values():
-            if key not in stats:
-                print("ERROR")
-        for key in kwargs.values():
-            if key in stats:
-                func[key](args)
+    for key in kwargs.values():
+        if key in stats:
+            func[key](args)
