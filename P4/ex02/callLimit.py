1b998a7f980105d5b8c441677c78dbf2c4a4db59
belen-applymed <158149022+belen-applymed@users.noreply.github.com>
2024-12-20 10:08:33 +0100

    p4 ex02 done

--- P4/ex02/callLimit.py
+++ P4/ex02/callLimit.py
@@ -1,5 +1,23 @@
 def callLimit(limit: int):
+    """
+    Receives a limit and returns a decorator
+    """
     count = 0
     def callLimiter(function):
-        def limit_function(*args: Any, **kwds: Any):
-            #your code here
+        """
+        Receives a function and returns a wrapper
+        """
+        def limit_function(*args: any, **kwds: any):
+            """
+            Wrapper that receives the args.
+            Returns the function if the count is lower than the limit.
+            Prints an error if the count is bigger than the limit.
+            """
+            nonlocal count
+            count += 1
+            if limit >= count:
+                return function(*args, **kwds)
+            else:
+                print(f"Error: {function} call too many times")
+        return limit_function
+    return callLimiter
